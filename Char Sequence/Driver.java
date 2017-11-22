3949f177dc3d556ae45119ae7c4281d89ff2bd85
fluffypancakes159 <cdiep@stuy.edu>
2017-11-22 14:42:50 -0500

    Char Sequence lab thingy

---
+++ Char Sequence/Driver.java
@@ -0,0 +1,11 @@
+public class Driver {
+
+    public static void main ( String[]args ) {
+	CharSequence A = new CharSequence ( "catfish" );
+	System.out.println ( A.charAt( 2 ) );
+	System.out.println ( A.length( ) );
+	System.out.println ( A.subSequence( 2 , 4 ) );
+	System.out.println ( A.toString( ) );
+    }
+    
+}
