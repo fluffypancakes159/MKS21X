e889d1cf2c11c97c91271ef805441b9c0ab15109
fluffypancakes <cdiep@stuy.edu>
2018-01-04 22:08:09 -0500

    added a constructor for a list input, fixed all of the tests except one janky one and binary search function

--- 02OrderedSuperArray/OrderedSuperArray.java
+++ 02OrderedSuperArray/OrderedSuperArray.java
@@ -1,11 +1,28 @@
 public class OrderedSuperArray extends SuperArray {
     
+	public static void main(String[] args) {
+		OrderedSuperArray A = new OrderedSuperArray ( 100 );
+		for(int i = 0; i < 100;   i ++){
+      	    String v = ""+(int)(Math.random()*100);
+      	    A.add(v);
+      	    // System.out.println( A );
+     	}
+     	System.out.println( A );
+	}
+
     public OrderedSuperArray ( ) {
-	new OrderedSuperArray ( 10 );
+		new OrderedSuperArray ( 10 );
     }
 
     public OrderedSuperArray ( int capacity ) {
 	super ( capacity );
+    }
+
+    public OrderedSuperArray ( String [] stuff ) {
+    	new OrderedSuperArray ( stuff.length );
+    	for ( int i = 0 ; i < stuff.length ; i++ ) {
+    		add( stuff[i] );
+    	}
     }
 
     public boolean add ( String element ) {
@@ -30,36 +47,28 @@ public class OrderedSuperArray extends SuperArray {
     }
     
     private int indexSearchBinary ( String element ) {
-	int start = 0;
-	int end = size( ) - 1;
-	int i;
-	if ( end == -1 ) { //there is probably a more elegant way
-	    return 0;      //to go about these cases
-	    }
-	if ( element.compareTo( get( end ) ) > 0 ) {
-	    return size( );
-	}
-	while ( start != end ) {
-	    i = ( start + end ) / 2;
-	    if ( element.compareTo( get( i ) ) > 0 ) {
-		if ( start == i ) {
-		    start++;
+		int start = 0;
+		int end = size( ) - 1;
+		int i;
+		while ( start <= end ) {
+		    i = ( start + end ) / 2;
+		    if ( element.compareTo( get ( i ) ) > 0 ) {
+				start = i + 1;
+	 	    }
+		    else if ( element.compareTo( get ( i ) ) < 0 ) {
+				end = i - 1;
+		    }
+	  	    else {
+				while ( i > 0 && get ( i - 1 ) == element ) { 
+					i -= 1;
+				}
+				return i;
+	  		}
 		}
-		else {
-		    start = i;
-		}
-	    }
-	    else if ( element.compareTo( get( i ) ) < 0 ) {
-		end = i;
-	    }
-	    else {
-		return i;
-	    }
-	}
-	return start;
+		return start;
     }
 
     public String set ( int index , String element ) {
-	throw new UnsupportedOperationException ( );
+		throw new UnsupportedOperationException ( );
     }
 }
